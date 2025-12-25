b80aefbf5806a099d62b6e21a422097120a7025a
csendin <eucaiosendin@gmail.com>
2025-12-25 10:43:09 -0300

    feat: setup front-end with Vite and TanStack Query

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,12 +1,27 @@
 import './index.css'
 
+import { createRouter, RouterProvider } from '@tanstack/react-router'
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 
-import { App } from './app'
+import { routeTree } from './routeTree.gen'
 
-createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <App />
-    </StrictMode>,
-)
+const router = createRouter({ routeTree })
+
+declare module '@tanstack/react-router' {
+    interface Register {
+        router: typeof router
+    }
+}
+
+// biome-ignore lint/style/noNonNullAssertion: mandatory by React
+const rootElement = document.getElementById('root')!
+
+if (!rootElement.innerHTML) {
+    const root = ReactDOM.createRoot(rootElement)
+    root.render(
+        <StrictMode>
+            <RouterProvider router={router} />
+        </StrictMode>
+    )
+}
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,15 @@
-import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        tanstackRouter({
+            target: 'react',
+            autoCodeSplitting: true,
+        }),
+        react(),
+        tailwindcss(),
+    ],
 })
